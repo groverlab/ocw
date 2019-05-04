891586b975d69abf1198226925cb7ce0b71a9257
William Grover <wgrover@engr.ucr.edu>
2019-05-03 21:34:05 -0700

    parses the open and close commands

--- arduino/ocw/ocw.ino
+++ arduino/ocw/ocw.ino
@@ -1,18 +1,26 @@
 void setup() {
-  pinMode(2, OUTPUT);
-  pinMode(3, OUTPUT);
-  pinMode(4, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(13, OUTPUT);
 }
 
-String ocw = String("o2 c4 w500 o3 c2 w500 o4 c3 w500");
+String ocw = String("o11 c13 w500 o12 c11 w500 o13 c12 w500");
 
 int i = 0;
 void loop() {
   if(ocw.charAt(i) == 'o') {
-    digitalWrite(ocw.substring(i+1,i+2).toInt(), HIGH);
+    int len = 1;
+    while(isDigit(ocw.charAt(i+1+len))) {
+      len += 1;
+    }
+    digitalWrite(ocw.substring(i+1,i+1+len).toInt(), HIGH);
   }
   if(ocw.charAt(i) == 'c') {
-    digitalWrite(ocw.substring(i+1,i+2).toInt(), LOW);
+    int len = 1;
+    while(isDigit(ocw.charAt(i+1+len))) {
+      len += 1;
+    }
+    digitalWrite(ocw.substring(i+1,i+1+len).toInt(), LOW);
   }
   if(ocw.charAt(i) == 'w') {
     int len = 1;
