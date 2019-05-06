c004ec7644b7d337c59d1dafb8b041c32c0cb78d
William Grover <wgrover@engr.ucr.edu>
2019-05-06 14:10:09 -0700

    autoreformat of code for switch cases

--- arduino/ocw/ocw.ino
+++ arduino/ocw/ocw.ino
@@ -28,127 +28,127 @@ unsigned long startTime = 0;
 
 void loop() {
   if (irrecv.decode(&results)) {
-      if (results.value == 0XFFFFFFFF) {
-        results.value = key_value;
-      }
-    switch(results.value) {
+    if (results.value == 0XFFFFFFFF) {
+      results.value = key_value;
+    }
+    switch (results.value) {
 
       case 0xFFA25D:  // POWER
-      break;
+        break;
 
       case 0xFF629D:  // VOL+
-      break;
+        break;
 
       case 0xFFE21D:  // FUNC/STOP
-      break;
+        break;
 
       case 0xFF22DD:  // |<<
-      lcd.setCursor(0, 0);
-      lcd.print("Pumping backward");
-      ocw = "o2 w200 c4 w200 o3 w200 c2 w200 o4 w200 c3 w200";
-      i=-1;
-      break;
+        lcd.setCursor(0, 0);
+        lcd.print("Pumping backward");
+        ocw = "o2 w200 c4 w200 o3 w200 c2 w200 o4 w200 c3 w200";
+        i = -1;
+        break;
 
       case 0xFF02FD:  // >||
-      lcd.setCursor(0, 0);
-      lcd.print("Standby         ");
-      ocw = "c2 c3 c4 w100";
-      i=-1;
-      break;  
+        lcd.setCursor(0, 0);
+        lcd.print("Standby         ");
+        ocw = "c2 c3 c4 w100";
+        i = -1;
+        break;
 
       case 0xFFC23D:  // >>|
-      lcd.setCursor(0, 0);
-      lcd.print("Pumping forward ");
-      ocw = "o4 w200 c2 w200 o3 w200 c4 w200 o2 w200 c3 w200";
-      i=0-1;
-      break;
+        lcd.setCursor(0, 0);
+        lcd.print("Pumping forward ");
+        ocw = "o4 w200 c2 w200 o3 w200 c4 w200 o2 w200 c3 w200";
+        i = 0 - 1;
+        break;
 
       case 0xFFE01F:  // v
-      break;
+        break;
 
       case 0xFFA857:  // VOL-
-      break; 
+        break;
 
       case 0xFF906F:  // ^
-      break; 
+        break;
 
       case 0xFF6897:  // 0
-      break;
+        break;
 
       case 0xFF9867:  // EQ
-      break;
+        break;
 
       case 0xFFB04F:  // ST/REPT
-      break;
+        break;
 
       case 0xFF30CF:  // 1
-      break;
+        break;
 
       case 0xFF18E7:  // 2
-      break;
+        break;
 
       case 0xFF7A85:  // 3
-      break;
+        break;
 
       case 0xFF10EF:  // 4
-      break;
+        break;
 
       case 0xFF38C7:  // 5
-      break;
+        break;
 
       case 0xFF5AA5:  // 6
-      break;
+        break;
 
       case 0xFF42BD:  // 7
-      break;
+        break;
 
       case 0xFF4AB5:  // 8
-      break;
+        break;
 
       case 0xFF52AD:  // 9
-      break;   
+        break;
 
     }
     key_value = results.value;
     irrecv.resume();
   }
-  
-  if(waiting) {
-    if(millis() >= startTime) {
+
+  if (waiting) {
+    if (millis() >= startTime) {
       waiting = false;
       i += 1;
     }
   }
-  
+
   else {
-    if(ocw.charAt(i) == 'o') {
+    if (ocw.charAt(i) == 'o') {
       int len = 1;
-      while(isDigit(ocw.charAt(i+1+len))) {
+      while (isDigit(ocw.charAt(i + 1 + len))) {
         len += 1;
       }
-      digitalWrite(ocw.substring(i+1,i+1+len).toInt(), HIGH);
+      digitalWrite(ocw.substring(i + 1, i + 1 + len).toInt(), HIGH);
     }
-    if(ocw.charAt(i) == 'c') {
+    if (ocw.charAt(i) == 'c') {
       int len = 1;
-      while(isDigit(ocw.charAt(i+1+len))) {
+      while (isDigit(ocw.charAt(i + 1 + len))) {
         len += 1;
       }
-      digitalWrite(ocw.substring(i+1,i+1+len).toInt(), LOW);
+      digitalWrite(ocw.substring(i + 1, i + 1 + len).toInt(), LOW);
     }
-    if(ocw.charAt(i) == 'w') {
+    if (ocw.charAt(i) == 'w') {
       int len = 1;
-      while(isDigit(ocw.charAt(i+1+len))) {
+      while (isDigit(ocw.charAt(i + 1 + len))) {
         len += 1;
       }
-      startTime = millis()+ocw.substring(i+1,i+1+len).toInt();
+      startTime = millis() + ocw.substring(i + 1, i + 1 + len).toInt();
       waiting = true;
     }
     i += 1;
-    if(i > ocw.length()) {
+    if (i > ocw.length()) {
       i = 0;
     }
   }
 
-lcd.setCursor(0, 1);
-lcd.print(millis()/1000.0);
+  lcd.setCursor(0, 1);
+  lcd.print(millis() / 1000.0);
 }
